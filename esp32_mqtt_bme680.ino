12a0bb7a9e6e55c0a98dcc68e6cd06326e3d4a98
Jacob Shufro <jacob@shuf.ro>
2020-08-04 16:12:55 -0400

    Liquid sensor only version

--- esp32_mqtt_bme680.ino
+++ esp32_mqtt_bme680.ino
@@ -1,6 +1,5 @@
 /*
  * Error              Meaning             Action
- * LED flashes once   BME680 not found    Make sure the BME680 is wired in and working. If using a non-standard I2C mode, you may have to update the BME680_I2C_MODE configuration.
  * LED flashes twice  WiFi error          Make sure wifi can reach the ESP32, and the NETWORK_ID/PASSWORD configuration is correct.
  * LED flashes trebly MQTT client error   Make sure your MQTT broker is running and the connection settings are correct.
  * Solid Light        No error!           Have some tea. Relax. Read a book.
@@ -11,9 +10,6 @@
 /* CONFIG WiFi */
 #define NETWORK_ID "YOUR WIFI GOES HERE"
 #define PASSWORD "YOUR WIFI PASSWORD GOES HERE"
-
-/* CONFIG BME680 */
-#define BME680_I2C_MODE I2C_STANDARD_MODE
 
 /* CONFIG MQTT */
 #define MQTT_TOPIC "house_env"
@@ -32,14 +28,10 @@
 
 /* CONFIG general */
 #define DEBUG 1
-#define BME680_ATTEMPTS 5U
-#define CONVERT_TO_FAHRENHEIT //Comment this out for Celsius
 #define INTERVAL 60000 //Milliseconds between measurements/publishings.
 #define SERIAL_SPEED 115200
 /* ---------- END of configuration area ---------- */
 
-
-#include "Zanshin_BME680.h"
 #include <WiFi.h>
 #include "mqtt_client.h"
 
@@ -53,7 +45,6 @@
 #endif
 
 
-BME680_Class BME680;
 esp_mqtt_client_config_t mqtt_config = {};
 static uint8_t soft_errors = 0;
 
@@ -81,34 +72,6 @@ error_loop(uint8_t blips)
   }
   PRINT("CRITICAL - Flashed error code 20 times. Resetting device.");
   ESP.restart();
-}
-
-void
-setup_BME680(void)
-{
-  uint8_t attempts = BME680_ATTEMPTS;
-
-  PRINT("- Initializing BME680 sensor...");
-  for (;;) {
-    if(BME680.begin(BME680_I2C_MODE)) {
-      BME680.setOversampling(TemperatureSensor, Oversample16);
-      BME680.setOversampling(HumiditySensor, Oversample16);
-      BME680.setOversampling(PressureSensor, Oversample16);
-      BME680.setIIRFilter(IIR4);
-      BME680.setGas(320,150);
-      return;
-    }
-
-    if (--attempts == 0) {
-      PRINT("CRITICAL - couldn't find BME680 after " STR(BME680_ATTEMPTS) "attempts.");
-      error_loop(1);
-      __builtin_unreachable();
-    }
-
-    PRINT("WARNING - couldn't find BME680. Retrying in 2 seconds.");
-    delay(2000);
-  }
-  __builtin_unreachable();
 }
 
 void
@@ -175,8 +138,7 @@ setup() {
   Serial.begin(SERIAL_SPEED);
   pinMode(LED_BUILTIN, OUTPUT);
   setup_liquid_sensor();
-  PRINT("Starting ESP32 Thing I2C-MQTT Environmental Sensor.");
-  setup_BME680();
+  PRINT("Starting ESP32 Thing I2C-MQTT Liquid Sensor.");
   setup_wifi();
   mqttc = setup_mqtt();
   PRINT("Setup done\n");
@@ -198,25 +160,6 @@ mqtt_publish(void)
   char buf[32];
   PRINTF("Publishing to MQTT broker... soft errors %d", soft_errors);
 
-  /* Temperature */
-#ifdef CONVERT_TO_FAHRENHEIT
-  temp = ((temp * 9) / 5) + 3200;
-#endif
-  snprintf(buf, sizeof(buf), "%d.%02d", temp / 100, temp % 100);
-  soft_errors += esp_mqtt_client_publish(mqttc, MQTT_TOPIC "/temp", buf, strlen(buf), MQTT_QOS, MQTT_RETAIN) == 0;
-
-  /* humidity */
-  snprintf(buf, sizeof(buf), "%d.%02d", humidity / 1000, (humidity % 1000)/10);
-  soft_errors += esp_mqtt_client_publish(mqttc, MQTT_TOPIC "/humidity", buf, strlen(buf), MQTT_QOS, MQTT_RETAIN) == 0;
-
-  /* pressure */
-  snprintf(buf, sizeof(buf), "%d", pressure);
-  soft_errors += esp_mqtt_client_publish(mqttc, MQTT_TOPIC "/pressure", buf, strlen(buf), MQTT_QOS, MQTT_RETAIN) == 0;
-
-  /* gas */
-  snprintf(buf, sizeof(buf), "%d", gas);
-  soft_errors += esp_mqtt_client_publish(mqttc, MQTT_TOPIC "/gas", buf, strlen(buf), MQTT_QOS, MQTT_RETAIN) == 0;
-
   /* wet/dry */
   snprintf(buf, sizeof(buf), "%s", digitalRead(LIQUID_SENSOR_GPIO) == 1 ? "dry" : "wet");
   soft_errors += esp_mqtt_client_publish(mqttc, MQTT_TOPIC "/liquid", buf, strlen(buf), MQTT_QOS, MQTT_RETAIN) == 0;
@@ -227,13 +170,11 @@ mqtt_publish(void)
 void
 loop()
 {
-  if (soft_errors >= 10) {
+  if (soft_errors >= 2) {
     PRINT("WARNING - encountered 10 non-fatal errors since reset, resetting");
     ESP.restart();
   }
   PRINT("ESP32 Thing I2C-MQTT Environmental Sensor Main Loop.");
-  BME680.getSensorData(temp,humidity,pressure,gas,true);
-  PRINTF("Temp: %d celsius, humidity: %d percent, pressure: %d Pascals, gas: %d ohms", temp, humidity / 1000, pressure, gas);
   mqtt_publish();
   delay(INTERVAL);
 }
